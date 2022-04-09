4b81119b39f7900263c1c29db6e258981e09c957
Ideja Ferati <idejaferati03@gmail.com>
2022-04-09 14:48:07 +0200

    multiple client communication

--- tcp-client.js
+++ tcp-client.js
@@ -2,28 +2,36 @@
 const Net = require('net');
 // The port number and hostname of the server.
 const port = 8080;
-const host = '127.0.0.1';
+const host = '127.0.0.1'; 
 
 // Create a new TCP client.
-const client = new Net.Socket();
+const client = new Net.Socket(); 
+
 // Send a connection request to the server.
-client.connect({ port: port, host: host }, function() {
+client.connect(port, host, function() { 
     // If there is no error, the server has accepted the request and created a new 
-    // socket dedicated to client1.
-    console.log('TCP connection established with the server.');
+    // socket dedicated to client.
+    console.log(`TCP connection established with the server on ${host}:${port}`+'\n'); 
 
     // The client can now send data to the server by writing to its socket.
-    client.write('Hello, server.');
-});
+    client.write(`Hello, server. I am ${client.address().address}.`+'\n');
+}); 
 
 // The client can also receive data from the server by reading from its socket.
-client.on('data', function(chunk) {
-    console.log(`Data received from the server: ${chunk.toString()}.`);
-    
-    // Request an end to the connection after the data has been received.
-    client.end();
-});
+client.on('data', function(chunk) {  
+    console.log(`Data received from the server: ${chunk.toString()}`+'\n'); 
 
-client.on('end', function() {
-    console.log('Requested an end to the TCP connection');
-});
+    // Request an end to the connection after entering exit in console
+    if (chunk.toString().endsWith('exit')) { 
+        client.destroy(); 
+    } 
+}); 
+
+// Add a 'close' event handler for the client socket 
+client.on('close', () => { 
+    console.log('Client closed'+'\n'); 
+}); 
+
+client.on('error', (err) => { 
+    console.error(err); 
+}); 
--- tcp-server.js
+++ tcp-server.js
@@ -1,41 +1,53 @@
 // Include Nodejs' net module.
 const Net = require('net');
 // The port on which the server is listening.
-const port = 8080;
+const port = 8080; 
+const host = '127.0.0.1'; 
 
 // Create a new TCP server.
 const server = Net.createServer(function(socket) {
-	socket.write('Echo server created\r\n');
-	socket.pipe(socket);
-});
+    socket.write('Echo server created\r\n');
+    socket.pipe(socket);
+}); 
 
 // The server listens to a socket for a client to make a connection request.
-server.listen(port, function() {
-    console.log(`Server listening for connection requests on socket 127.0.0.19:${port}`);
-});
+server.listen(port, host, function() { 
+    console.log(`Server listening for connection requests on socket ${host}:${port}`+'\n'); 
+}); 
 
+let sockets = []; 
 // When a client requests a connection with the server, the server creates a new
 // socket dedicated to that client.
-server.on('connection', function(socket) {
-    console.log('A new connection has been established.');
-
+server.on('connection', function(socket) { 
+    var clientAddress = `${socket.remoteAddress}:${socket.remotePort}`; 
+    console.log(`A new connection has been established. Client on ${clientAddress}`+'\n'); 
+    sockets.push(socket); 
     // Now that a TCP connection has been established, the server can send data to
     // the client by writing to its socket.
-    socket.write('Hello, client number 1');
+    socket.write(`Hello, client ${clientAddress}`);
 
-    // The server can also receive data from the client by reading from its socket.
-    socket.on('data', function(chunk) {
-        console.log(`Data received from client 1: ${chunk.toString()}`);
-    });
+    socket.on('data', function(data) {  
+        // Write the data back to all the connected, the client will receive it as data from the server 
+        sockets.forEach(function(sock) { 
+            sock.write(socket.remoteAddress + ':' + socket.remotePort + " said " + data + '\n'); 
+        }); 
+    }); 
 
-    // When the client requests to end the TCP connection with the server, the server
-    // ends the connection.
-    socket.on('end', function() {
-        console.log('Closing connection with the client');
-    });
+    // Add a 'close' event handler to this instance of socket 
+    socket.on('close', function(data) { 
+        let index = sockets.findIndex(function(o) { 
+            return o.remoteAddress === socket.remoteAddress && o.remotePort === socket.remotePort; 
+        }) 
 
-    // In case errors happen.
+        if (index !== -1) sockets.splice(index, 1); 
+        sockets.forEach((sock) => { 
+            sock.write(`${clientAddress} disconnected\n`); 
+        }); 
+        console.log(`connection closed: ${clientAddress}`+'\n'); 
+    }); 
+
+    // In case errors happen
     socket.on('error', function(err) {
         console.log(`Error: ${err}`);
-    });
-});
+    }); 
+}); 
