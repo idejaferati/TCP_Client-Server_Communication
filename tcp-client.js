4bce709b4a173c5fd2898f49b7adf2d7d09e9c52
Ideja Ferati <idejaferati03@gmail.com>
2022-04-11 14:40:47 +0200

    Main changes in read/write/execute file

--- tcp-client.js
+++ tcp-client.js
@@ -1,34 +1,32 @@
 // Include Nodejs' net module.
 const Net = require('net');
-const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require('prompt-sync')();
 // The port number and hostname of the server.
-const port = 8080;
-const host = prompt("Please write the IP address you want to connect with: "); 
+const port = 8484;
+const host = '192.168.178.49'; 
 
 // Create a new TCP client.
 const client = new Net.Socket(); 
 
 // Send a connection request to the server.
-client.connect(port, host, function() { 
+client.connect(port, host,function() { 
     // If there is no error, the server has accepted the request and created a new 
     // socket dedicated to client.
     console.log(`TCP connection established with the server on ${host}:${port}`+'\n'); 
 
-    // The client can now send data to the server by writing to its socket.
-    var text = prompt("Username and password: ");
+    var text = prompt("Write something: ");
     client.write(text);
+    
+    // The client can also receive data from the server by reading from its socket.
+    client.on('data', function(chunk) {  
+        console.log(`Data received from the server: ${chunk.toString()}`+'\n'); 
+        
+        // Request an end to the connection after entering exit in console
+        if (chunk.toString().endsWith('exit')) { 
+            client.destroy(); 
+        } 
+    });
 }); 
-
-// The client can also receive data from the server by reading from its socket.
-client.on('data', function(chunk) {  
-    console.log(`Data received from the server: ${chunk.toString()}`+'\n'); 
-
-    // Request an end to the connection after entering exit in console
-    if (chunk.toString().endsWith('exit')) { 
-        client.destroy(); 
-    } 
-}); 
-
 
 // Add a 'close' event handler for the client socket 
 client.on('close', () => { 
